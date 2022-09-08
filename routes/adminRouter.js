8721027f1c64a68d506e2514ea5aef8b903b53ad
handi7 <handi7.co@gmail.com>
2022-09-08 15:31:12 +0700

    Admin forgot & reset password

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,13 +4,20 @@ import {
   keepLogin,
   addAdmin,
   verification,
+  sendResetLink,
+  authResetToken,
+  resetPassword,
 } from "../controller/adminController.js";
 import { AuthToken } from "../helper/authToken.js";
+
 const routers = express.Router();
 
 routers.post("/login", login);
 routers.post("/keepLogin", keepLogin);
 routers.post("/add-admin", addAdmin);
+routers.post("/forgot-password", sendResetLink);
+routers.post("/auth-reset-token", AuthToken, authResetToken);
 routers.patch("/verification", AuthToken, verification);
+routers.patch("/reset-password", resetPassword);
 
 export default routers;
